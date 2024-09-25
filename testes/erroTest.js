6372f0bd4b488ef15f068391836e69ee287c4cb9
ElisIrons <elisirons96@gmail.com>
2024-09-25 15:57:19 -0300

    Alterando url do teste playwright

--- testes/erroTest.js
+++ testes/erroTest.js
@@ -4,17 +4,17 @@ const { chromium } = require("playwright");
   const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("file:///C:/Users/Elis/jogoNumeroSecreto/index.html");
+  await page.goto("https://elisirons.github.io/jogoNumeroSecreto/");
 
   const title = await page.title();
   console.log("Título da página:", title);
 
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
 
   await page.fill('input[type="number"]', "15");
   await page.click('button:has-text("Chutar")');
 
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
 
   const errorMessage = await page.locator(".texto__paragrafo").innerText();
   if (errorMessage.includes("Por favor, insira um número entre 1 e 10.")) {
@@ -23,7 +23,7 @@ const { chromium } = require("playwright");
     console.log("Erro: A mensagem de erro não foi exibida.");
   }
 
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
 
   await browser.close();
 })();
